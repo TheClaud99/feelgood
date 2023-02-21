b99470ef1405d42951749461e16bef50d81a7faa
c.mano <c.mano@progresso.srl>
2023-02-21 23:33:45 +0100

    Ritornati eventi da HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Member;
 use App\Models\Product;
+use App\Models\Event;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -18,6 +19,7 @@ class HomeController extends Controller
     {
         $team_members = Member::active()->get();
         $products = Product::active()->get();
-        return Inertia::render('Homepage', ['members' => $team_members, 'products' => $products]);
+        $events = Event::active()->get();
+        return Inertia::render('Homepage', ['members' => $team_members, 'products' => $products, 'events' => $events]);
     }
 }
