6faf0854bfedbabeb653e7e3636e3e1c78048d8a
c.mano <c.mano@progresso.srl>
2023-03-09 18:51:50 +0100

    Inserita opacità immagine sfondo curriculum

--- resources/js/Components/Curriculum.vue
+++ resources/js/Components/Curriculum.vue
@@ -1,5 +1,6 @@
 <script setup>
 import "bootstrap";
+import isEqual from "lodash.isequal";
 defineProps({
     member: {
         type: Object,
@@ -9,15 +10,14 @@ defineProps({
 
 <template>
     <div
-        v-if="member"
         class="modal fade show"
         tabindex="-1"
-        aria-labelledby="exampleModalFullscreenLabel"
+        aria-labelledby="curriculum"
         aria-modal="true"
         role="dialog"
     >
         <div class="modal-dialog modal-fullscreen">
-            <div class="modal-content">
+            <div class="modal-content" v-if="member && !isEqual(member, {})">
                 <div class="modal-header">
                     <h5 class="modal-title">{{ member.name }}</h5>
                     <button
@@ -27,8 +27,15 @@ defineProps({
                         aria-label="Close"
                     ></button>
                 </div>
-                <div class="modal-body">
-                    <span v-html="member.description"></span>
+                <div class="modal-body position-relative overflow-hidden">
+                    <img
+                        :src="`/storage/${member.image}`"
+                        class="position-absolute overflow-hidden h-100 d-block"
+                    />
+                    <span
+                        class="d-block h-100 overflow-auto position-relative"
+                        v-html="member.description"
+                    ></span>
                 </div>
                 <div class="modal-footer"></div>
             </div>
@@ -40,4 +47,20 @@ defineProps({
 @import "../node_modules/bootstrap/scss/functions";
 @import "../node_modules/bootstrap/scss/variables";
 @import "../node_modules/bootstrap/scss/mixins";
+
+.modal-body {
+    img {
+        bottom: 0;
+        opacity: 0.7;
+
+        @include media-breakpoint-up(xs) {
+            left: 0;
+        }
+
+        @include media-breakpoint-up(lg) {
+            left: auto;
+            right: 20px;
+        }
+    }
+}
 </style>
