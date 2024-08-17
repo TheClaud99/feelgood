9253630b75c8456bd590b12be7e3df9c1a432dd3
c.mano <c.mano@progresso.srl>
2024-08-17 17:10:12 +0200

    Aggiunte icone backpack

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -7,7 +7,7 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('post') }}"><i class="nav-icon la la-question"></i> Posts</a></li> -->
 
 <x-backpack::menu-item title="Dashboard" icon="la la-dashboard" :link="backpack_url('dashboard')" />
-<x-backpack::menu-item title="Members" icon="la la-question" :link="backpack_url('member')" />
-<x-backpack::menu-item title="Events" icon="la la-question" :link="backpack_url('event')" />
-<x-backpack::menu-item title="Products" icon="la la-question" :link="backpack_url('product')" />
-<x-backpack::menu-item title="Posts" icon="la la-question" :link="backpack_url('post')" />
+<x-backpack::menu-item title="Members" icon="la la-user" :link="backpack_url('member')" />
+<x-backpack::menu-item title="Events" icon="la la-calendar" :link="backpack_url('event')" />
+<x-backpack::menu-item title="Products" icon="la la-shopping-bag" :link="backpack_url('product')" />
+<x-backpack::menu-item title="Posts" icon="la la-newspaper" :link="backpack_url('post')" />
