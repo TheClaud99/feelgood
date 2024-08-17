cefb408decc6c6612f703a6a96531aa97600e2cc
c.mano <c.mano@progresso.srl>
2024-08-17 16:07:41 +0200

    Upgrade to laravel 11

--- database/migrations/2023_02_20_174209_create_products_table.php
+++ database/migrations/2023_02_20_174209_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->longText('description')->nullable();
             $table->string('image')->nullable();
             $table->longText('photos')->nullable();
-            $table->float('price', 8, 2)->nullable();
+            $table->float('price')->nullable();
             $table->integer('sequence')->nullable();
             $table->boolean('active')->default(1);
             $table->timestamps();
